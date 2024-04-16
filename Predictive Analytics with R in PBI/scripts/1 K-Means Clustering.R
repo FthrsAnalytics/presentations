4e90967b6f9c8f33c17c8591072ca3400671f8d2
Joseph Yeates <34662783+FthrsAnalytics@users.noreply.github.com>
2024-04-15 23:58:35 -0700

    Added additional presentation files

--- Predictive Analytics with R in PBI/scripts/1 K-Means Clustering.R
+++ Predictive Analytics with R in PBI/scripts/1 K-Means Clustering.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 #Import Data
 #This needs to point to the location of the Source Data.xlsx file
-file_path <- "C:/Users/jfthr/Desktop/R in PBI/Excel Source Data.xlsx"
+file_path <- "C:/Users/jfthr/Documents/GitHub/presentations/Predictive Analytics with R in PBI/data/Excel Source Data.xlsx"
 
 data_set <- read_excel(file_path) %>% 
   select(Id, Total_Revenue, Income, Selected_By_Percent, Days_Worked, Aff_Client_Meetings, Client_Meetings, Admin, Position_Short)
